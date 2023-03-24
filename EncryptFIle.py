cac1814259f3b8171de61b5c281fa7d363d65586
francsir <francisr@tcd.ie>
2023-03-24 12:27:22 +0000

    Added .gitignore, Encrypt files work

--- EncryptFIle.py
+++ EncryptFIle.py
@@ -1,6 +1,16 @@
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.backends import default_backend
 
 def encrypt(filename, public_key_filename):
     with open(public_key_filename, 'rb') as key_file:
-        public_key = serialization.load_pem_public_key(key_file.read(), backend=,)
+        public_key = serialization.load_pem_public_key(key_file.read(), backend=default_backend(),)
+
+    with open(filename, 'rb') as f:
+        plaintext = f.read()
+    cipher = public_key.encrypt(plaintext, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label = None,))
+
+    with open(filename + '.encrypted', 'wb') as f:
+        f.write(cipher)
+
+encrypt("test.txt", "public_key.pem")
