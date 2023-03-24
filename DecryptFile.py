c85360f4df9ec00fa0e742063879d915ff5b4d8f
francsir <francisr@tcd.ie>
2023-03-24 12:37:23 +0000

    Decrypting of files works

---
+++ DecryptFile.py
@@ -0,0 +1,18 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.backends import default_backend
+
+
+def Decrypt(filename, private_key_filename):
+    with open(private_key_filename, 'rb') as f:
+        private_key = serialization.load_pem_private_key(f.read(), password = None, backend = default_backend())
+
+    with open(filename, 'rb') as f:
+        cipher = f.read()
+
+    plaintext = private_key.decrypt(cipher, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+
+    with open(filename + '.decrypted', 'wb') as f:
+        f.write(plaintext)
+
+Decrypt("test.txt.encrypted", "private_key.pem")
